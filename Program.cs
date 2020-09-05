7f21978ac79d94b2f0c3d5d9a2a82420cc9c1cb9
CaueSouza <caue.souzabarros@hotmail.com>
2020-09-05 10:44:08 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace LexicoConsole
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string filePath, fileName;
+            Console.Write("Informe o caminho do arquivo: ");
+            filePath = Console.ReadLine();
+            Console.Write("Informe o nome do arquivo: ");
+            fileName = Console.ReadLine();
+
+            Console.WriteLine("Path: {0}\nName: {1}\n", filePath, fileName);
+
+            string fullPath = filePath + '\\' + fileName;
+            Console.WriteLine("Full Path: {0}", fullPath);
+
+            try
+            {
+                string[] lines = File.ReadAllLines(fullPath);
+
+                foreach (string line in lines)
+                {
+
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Impossivel ler o arquivo");
+            }
+        }
+    }
+}
