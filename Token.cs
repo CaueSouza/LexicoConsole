27948c8f5de2fff5719bcc09d0dbc48f8b7f9120
CaueSouza <caue.souzabarros@hotmail.com>
2020-09-06 17:18:18 -0300

    implementing token object

---
+++ Token.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LexicoConsole
+{
+    class Token
+    {
+        String simbolo;
+        String lexema;
+
+        public Token(string simbolo, string lexema)
+        {
+            this.simbolo = simbolo;
+            this.lexema = lexema;
+        }
+
+        public string getSimbolo() { return simbolo; }
+
+        public string getLexema() { return lexema; }
+    }
+}
