90e3dc1e37ca866cfe5b879fcdbef3dd23d834ad
CaueSouza <caue.souzabarros@hotmail.com>
2020-09-06 17:17:22 -0300

    descarting comments and spaces

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Security.Cryptography.X509Certificates;
 
 namespace LexicoConsole
 {
     class Program
     {
-        
+        private static char caracterAtual = ' ';
+        private static int cont = 0;
+        private static string fullString = "";
+        private static List<Token> listaTokens = new List<Token>();
+
         static void Main(string[] args)
         {
             string filePath, fileName;
@@ -19,19 +25,74 @@ namespace LexicoConsole
             string fullPath = filePath + '\\' + fileName;
             Console.WriteLine("Full Path: {0}", fullPath);
 
+            //using (StreamReader sr = File.OpenText(fullPath))
+            //{
+            //    string line;
+            //    while ((line = sr.ReadLine()) != null)
+            //    {
+            //        // Do something with line...
+            //        Console.WriteLine(line);
+            //    }
+            //}
+
+            readFile(fullPath);
+        }
+
+        private static void readFile(String fullPath)
+        {
             try
             {
                 string[] lines = File.ReadAllLines(fullPath);
 
                 foreach (string line in lines)
                 {
+                    fullString += " " + line;
+                }
 
+                caracterAtual = fullString[cont];
+
+                while (cont < fullString.Length)
+                {
+                    while ((caracterAtual == '{' || caracterAtual == ' ') && cont < fullString.Length)
+                    {
+                        if (caracterAtual == '{')
+                        {
+                            while (caracterAtual != '}' && cont < fullString.Length)
+                            {
+                                readCaracter();
+                            }
+
+                            readCaracter();
+                        }
+
+                        while (caracterAtual == ' ')
+                        {
+                            readCaracter();
+                        }
+                    }
+
+                    if (cont < fullString.Length)
+                    {
+                        //listaTokens.Add(readToken());
+                        readCaracter();//temporario
+                    }
                 }
             }
             catch (IOException e)
             {
                 Console.WriteLine("Impossivel ler o arquivo");
             }
+        }
+
+        private static void readCaracter()
+        {
+            cont++;
+            caracterAtual = fullString[cont];
+        }
+
+        private static Token readToken()
+        {
+            return null;
         }
     }
 }
